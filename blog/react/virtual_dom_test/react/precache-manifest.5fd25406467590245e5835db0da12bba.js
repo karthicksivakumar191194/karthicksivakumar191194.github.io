9578d34ab7b48abea52015a50aeaed100ef7b4fa
Karthick Sivakumar <karthicksivakumardev@gmail.com>
2021-09-28 18:50:55 +0530

    Initial Commit

---
+++ blog/react/virtual_dom_test/react/precache-manifest.5fd25406467590245e5835db0da12bba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0ad676d3792b8125f42da470174dc6",
+    "url": "/blog/react/virtual_dom_test/react/index.html"
+  },
+  {
+    "revision": "831adff0f749a4c63594",
+    "url": "/blog/react/virtual_dom_test/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33c83bd99dcfbda58619",
+    "url": "/blog/react/virtual_dom_test/react/static/js/2.2d628527.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blog/react/virtual_dom_test/react/static/js/2.2d628527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "831adff0f749a4c63594",
+    "url": "/blog/react/virtual_dom_test/react/static/js/main.b2f4e196.chunk.js"
+  },
+  {
+    "revision": "72bac09924f4c58a13c3",
+    "url": "/blog/react/virtual_dom_test/react/static/js/runtime-main.950fbd12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/react/virtual_dom_test/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
